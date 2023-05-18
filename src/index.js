3dd586a98a67094e0000d646f55ce425b723937e
clevercoderjoy <clevercoderjoy@gmail.com>
2023-05-18 22:59:32 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+
 import App from "./App";
 import { makeServer } from "./server";
 
 // Call make Server
 makeServer();
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <Router>
+            <App />
+        </Router>
+    </React.StrictMode>
 );
